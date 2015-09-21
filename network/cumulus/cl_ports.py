9434ded6ebc493d2e8a42f23ddf5db564db70c5a
Leslie Carr <geekgirl@gmail.com>
2015-09-21 15:36:30 -0700

    switching author line to github organization id

--- network/cumulus/cl_ports.py
+++ network/cumulus/cl_ports.py
@@ -18,7 +18,7 @@
 DOCUMENTATION = '''
 ---
 module: cl_ports
-author: "Cumulus Networks (ce-ceng@cumulusnetworks.com)"
+author: "Cumulus Networks (@CumulusNetworks)"
 short_description: Configure Cumulus Switch port attributes (ports.conf)
 description:
     - Set the initial port attribute defined in the Cumulus Linux ports.conf,
