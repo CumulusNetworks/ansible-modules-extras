0ef0421435cfb48b0def60193ebe01f85bddc9f0
Leslie Carr <geekgirl@gmail.com>
2015-09-21 16:18:01 -0700

    switching author line to github organization id

--- network/cumulus/cl_bond.py
+++ network/cumulus/cl_bond.py
@@ -18,7 +18,7 @@
 DOCUMENTATION = '''
 ---
 module: cl_bond
-author: "Cumulus Networks (ce-ceng@cumulusnetworks.com)"
+author: "Cumulus Networks (@CumulusNetworks)"
 short_description: Configures a bond port on Cumulus Linux
 description:
     - Configures a bond interface on Cumulus Linux To configure a bridge port
